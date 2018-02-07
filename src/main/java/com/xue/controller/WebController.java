a284f5707942549cbfa841a9cede851cf205444c
xues <xues@91wangjing.com>
2018-02-07 13:59:46 +0800

    Spring boot session 集群

--- src/main/java/com/xue/controller/WebController.java
+++ src/main/java/com/xue/controller/WebController.java
@@ -1,5 +1,7 @@
 package com.xue.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="/web")
 public class WebController {
 	
-	@RequestMapping(value="index")
+	/*@RequestMapping(value="index")
 	public String index(ModelMap map) {
 		map.put("title", "hello spring boot");
+		return "index";
+	}*/
+	
+	@RequestMapping(value = "/index")
+	public String index(ModelMap map, HttpSession httpSession) {
+		map.put("title", "第一个应用：sessionID=" + httpSession.getId());
+		System.out.println("sessionID=" + httpSession.getId());
 		return "index";
 	}
 	
